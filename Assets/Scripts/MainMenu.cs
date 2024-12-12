2d0a8f33e37a70dce92affea20182ae40c0ad69d
productsystem <prajitpradeep@gmail.com>
2024-12-12 20:11:06 +0530

    some spice, menu added

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
